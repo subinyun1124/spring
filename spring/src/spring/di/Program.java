e44303b49e9d40dfe73a4bd40b3056681612b46b
subinyun1124 <subinyun1124@gmail.com>
2025-06-20 16:36:32 +0900

    이노테이션 사용 전 xml 직접 DI 주입

--- spring/src/spring/di/Program.java
+++ spring/src/spring/di/Program.java
@@ -1,5 +1,11 @@
 package spring.di;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import spring.di.entity.Exam;
 import spring.di.entity.SubinExam;
 import spring.di.ui.ExamConsole;
@@ -10,17 +16,32 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		// 스프링에게 지시하는 방법으로 코드를 변ㄱ
+		// 스프링에게 지시하는 방법으로 코드를 변경
 //		Exam exam = new SubinExam();
 //		ExamConsole console = new GridExamConsole();
 //		
 //		console.setExam(exam);
 		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring/di/setting.xml");
 		
-		
-		ExamConsole console = ?;
+		Exam exam = context.getBean(Exam.class);
+		System.out.print(exam.toString());
+		//ExamConsole console = (ExamConsole) context.getBean("console"); //이름으로 가져오기, 형식변환 필
+		ExamConsole console = context.getBean(ExamConsole.class); //자료형  << 많이
 		
 		console.print();
+		
+//		List<Exam> exams = new ArrayList<>();
+//		exams.add(new SubinExam(1, 1, 1, 1));
+		
+		List<Exam> exams = (List<Exam>) context.getBean("exams");
+		//exams.add(new SubinExam(1, 1, 1, 1));
+		
+		for(Exam e : exams) {
+			System.out.println(e);
+		}
+	
+	
 	}
 	
 }
--- spring/src/spring/di/entity/SubinExam.java
+++ spring/src/spring/di/entity/SubinExam.java
@@ -5,11 +5,60 @@ public class SubinExam implements Exam {
 	private int kor;
 	private int eng;
 	private int math;
-	private int comp;
+	private int com;
+	
+	//기본생성자 
+	public SubinExam() {
+		// TODO Auto-generated constructor stub
+	}
 	
 	@Override
+	public String toString() {
+		return "SubinExam [kor=" + kor + ", eng=" + eng + ", math=" + math + ", com=" + com + "]";
+	}
+
+	public SubinExam(int kor, int eng, int math, int com) {
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.com = com;
+	}
+
+	@Override
 	public int total() {
-		return kor+eng+math+comp;
+		return kor+eng+math+com;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public int getCom() {
+		return com;
+	}
+
+	public void setCom(int com) {
+		this.com = com;
 	}
 
 	@Override
